6579f04278f3d62065c166362f714648b27fd50a
Shabbir Hussain <shabbir.ahussain@gmail.com>
2016-06-28 22:15:16 -0400

    Added robots rules

--- HW3/src/com/ir/homework/hw3/processes/WebCrawler.java
+++ HW3/src/com/ir/homework/hw3/processes/WebCrawler.java
@@ -5,6 +5,8 @@ import static com.ir.homework.hw3.Constants.DEQUEUE_SIZE;
 import static com.ir.homework.hw3.Constants.FIELD_DISCOVERY_TIME;
 import static com.ir.homework.hw3.Constants.MAX_BUFFER_SIZE;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.Collection;
 import java.util.Date;
@@ -12,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.apache.commons.io.IOUtils;
 import org.elasticsearch.search.SearchHit;
 import org.jsoup.Jsoup;
 import org.jsoup.Connection.Response;
@@ -21,9 +24,17 @@ import com.ir.homework.hw3.tools.DefaultTokenizer;
 import com.ir.homework.hw3.tools.WebPageParser;
 import com.ir.homework.hw3.tools.WebPageParser.ParsedWebPage;
 
+import crawlercommons.robots.BaseRobotRules;
+import crawlercommons.robots.SimpleRobotRules;
+import crawlercommons.robots.SimpleRobotRules.RobotRulesMode;
+import crawlercommons.robots.SimpleRobotRulesParser;
+
 public class WebCrawler extends Thread {
 	private static WebPageParser     _webPageParser;
 	private static Map<String, Long> _domainAccessTime;
+	private static SimpleRobotRulesParser _parser;
+	private static Map<String, BaseRobotRules> _robotRuleMap;
+	
 
 	private Collection<String> queryTerms;
 	private ElasticClient      elasticClient;
@@ -31,7 +42,9 @@ public class WebCrawler extends Thread {
 	
 	static{
 		_domainAccessTime = new ConcurrentHashMap<String, Long>();
-		_webPageParser    =  new WebPageParser();
+		_webPageParser    = new WebPageParser();
+		_parser           = new SimpleRobotRulesParser();
+		_robotRuleMap	  = new HashMap<String, BaseRobotRules>();
 	}
 	
 	/**
@@ -54,8 +67,6 @@ public class WebCrawler extends Thread {
 				SearchHit[] queue = elasticClient.dequeue(DEQUEUE_SIZE);
 				
 				if(queue.length == 0) {
-//					this.log("Flusing buffer...");
-//					elasticClient.flush();
 					Thread.sleep(2000);
 				}
 				
@@ -108,7 +119,8 @@ public class WebCrawler extends Thread {
 			
 			//this.log("Buffering2 [" + url + "]"+ parsedWebPage.outLinks.size());
 			for(URL link : parsedWebPage.outLinks){
-				elasticClient.enqueue(score, link, discoveryTime);
+				if(this.isCrawlingAllowed(link))
+					elasticClient.enqueue(score, link, discoveryTime);
 			}
 			
 
@@ -144,6 +156,38 @@ public class WebCrawler extends Thread {
 		}
 		
 		return okapi_tf;
+	}
+	
+	/**
+	 * Answers if crawling is allowed or not depending on robot rules
+	 * @param url is the url to check
+	 * @return Boolean shows if url can be crawled or not
+	 */
+	public boolean isCrawlingAllowed(URL url) {
+		String domain = url.getHost();
+		BaseRobotRules rules = _robotRuleMap.get(domain);
+		if(rules == null) {
+			rules = new SimpleRobotRules(RobotRulesMode.ALLOW_ALL);
+			try{
+				Response response = Jsoup.connect(domain + "/" + "robots.txt")
+						.userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.120 Safari/535.2")
+						.followRedirects(true)
+						.timeout(2000)
+						.ignoreHttpErrors(true)
+						.execute();
+
+				if(!(response.statusCode() == 404 || response == null)) {
+					String robotContent = response.parse().body().text();
+					InputStream content = new ByteArrayInputStream(robotContent.getBytes());
+					rules = _parser.parseContent(url.toString(), 
+							IOUtils.toByteArray(content), 
+							"text/plain", 
+							"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.120 Safari/535.2");
+					_robotRuleMap.put(domain, rules);
+				}
+			} catch(Exception e) {}
+		}
+		return rules.isAllowed(url.toString());
 	}
 	
 	/**
