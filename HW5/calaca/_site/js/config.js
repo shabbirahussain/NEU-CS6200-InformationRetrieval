627f6d75135e971e58e5887a289117f79f24f2a7
Shabbir Hussain <shabbir.ahussain@gmail.com>
2016-07-12 20:18:28 -0400

    HW5 calaca created

---
+++ HW5/calaca/_site/js/config.js
@@ -0,0 +1,28 @@
+/*
+ * Calaca - Search UI for Elasticsearch
+ * https://github.com/romansanchez/Calaca
+ * http://romansanchez.me
+ * @rooomansanchez
+ * 
+ * v1.2.0
+ * MIT License
+ */
+
+/* Configs */
+/**
+ *
+ * url - Cluster http url
+ * index_name - Index name or comma-separated list
+ * type - Type
+ * size - Number of results to display at a time when pagination is enabled.
+ * search_delay - Delay between actual search request in ms. Reduces number of queries to cluster by not making a request on each keystroke. 
+ */
+
+var CALACA_CONFIGS = {
+	url: "http://elastichost:9200",
+	index_name: "1512_great_mordenist_artist",
+	type: "document",
+	field: "TEXT",
+	size: 10,
+	search_delay: 500
+}
