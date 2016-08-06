609684824d3ed8f9a887c2544cdf8be438fa45d5
Shabbir Hussain <shabbir.ahussain@gmail.com>
2016-08-06 02:22:11 -0400

    Cleanup

---
+++ HW7/src/com/ir/homework/hw7/machinelearning/Test.java
@@ -0,0 +1,16 @@
+package com.ir.homework.hw7.machinelearning;
+
+
+import nz.ac.waikato.cms;
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public static void main(String args[]){
+		
+	}
+
+}
